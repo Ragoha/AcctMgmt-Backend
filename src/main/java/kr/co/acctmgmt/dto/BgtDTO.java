95c54d14fa612d939296601073efa31755f5bf47
Ragoha <ragoha92@naver.com>
2023-07-14 22:02:36 +0900

    Team Merge. 2023-07-14 (#11)
    Co-authored-by: Yindy07 <ghdus951@gmail.com>
    Co-authored-by: Yindy07 <125229498+Yindy07@users.noreply.github.com>
    Co-authored-by: Jaeung08 <129034152+Jaeung08@users.noreply.github.com>
    Co-authored-by: pshongi <84509855+pshongi@users.noreply.github.com>

---
+++ src/main/java/kr/co/acctmgmt/dto/BgtDTO.java
@@ -0,0 +1,13 @@
+package kr.co.acctmgmt.dto;
+
+import lombok.Data;
+
+@Data
+public class BgtDTO {
+	private String id;
+	private String bgtCd;		//예산 코드
+	private String bgtFg;		//예산 구분
+	private String bgtNm;		//예산 과목명
+	private Long amount;		//금액
+	
+}
