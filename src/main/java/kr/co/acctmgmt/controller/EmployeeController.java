c5661d85838005ce859a122d9a91cb00904f84cf
Yindy07 <ghdus951@gmail.com>
2023-07-17 19:15:49 +0900

    feat. 회원가입

--- src/main/java/kr/co/acctmgmt/controller/EmployeeController.java
+++ src/main/java/kr/co/acctmgmt/controller/EmployeeController.java
@@ -1,31 +1,31 @@
-package kr.co.acctmgmt.controller;
-
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import kr.co.acctmgmt.domain.Employee;
-import kr.co.acctmgmt.service.EmployeeService;
-import lombok.RequiredArgsConstructor;
-
-@RestController
-@RequiredArgsConstructor
-public class EmployeeController {
-
-	private final EmployeeService employeeService;
-	
-	@RequestMapping("/employee")
-	public ResponseEntity<Employee> getEmployee() {
-		
-		return new ResponseEntity<Employee>(employeeService.getEmployee(), HttpStatus.OK);
-	}
-	
-	@RequestMapping("/employees")
-	public ResponseEntity<List<Employee>> getEmployeeList(){
-		
-		return new ResponseEntity<List<Employee>>(employeeService.getEmployeeList(), HttpStatus.OK);
-	}
-}
+//package kr.co.acctmgmt.controller;
+//
+//import java.util.List;
+//
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import kr.co.acctmgmt.domain.Employee;
+//import kr.co.acctmgmt.service.EmployeeService;
+//import lombok.RequiredArgsConstructor;
+//
+//@RestController
+//@RequiredArgsConstructor
+//public class EmployeeController {
+//
+//	private final EmployeeService employeeService;
+//	
+//	@RequestMapping("/employee")
+//	public ResponseEntity<Employee> getEmployee() {
+//		
+////		return new ResponseEntity<Employee>(employeeService.getEmployee(), HttpStatus.OK);
+//	}
+//	
+//	@RequestMapping("/employees")
+//	public ResponseEntity<List<Employee>> getEmployeeList(){
+//		
+//		return new ResponseEntity<List<Employee>>(employeeService.getEmployeeList(), HttpStatus.OK);
+//	}
+//}
--- src/main/java/kr/co/acctmgmt/domain/Employee.java
+++ src/main/java/kr/co/acctmgmt/domain/Employee.java
@@ -6,17 +6,15 @@ import lombok.Data;
 @Data
 @Builder
 public class Employee {
-	private String coCode;
-	private String dId;
-	private String dPs;
-	private String dEmail;
-	private String dTel;
-	private String dName;
-	private String dBday;
-	private String dAdr;
-	private String dZp;
-	private String dSx;
-	private String dBcd;
-	private String dCd;
-	private String dOd;
+	
+	private String coCd;
+	private String empId;
+	private String empPs;
+	private String empEmail;
+	private String empTel;
+	private String empName;
+	private String empSx;
+	private String empCd;
+	private String empOd;
+	private String empAuth;
 }
