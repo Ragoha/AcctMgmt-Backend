2afb6b90d2d38be64e440241093c34fef00f09d6
Jaeung08 <129034152+Jaeung08@users.noreply.github.com>
2023-07-21 17:22:32 +0900

    feat. DB객체 생성

--- src/main/java/kr/co/acctmgmt/service/SBGTCDServiceImpl.java
+++ src/main/java/kr/co/acctmgmt/service/SBGTCDServiceImpl.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Service;
 
 import kr.co.acctmgmt.domain.SBGTCDDomain;
 import kr.co.acctmgmt.mapper.SBGTCDMapper;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class SBGTCDServiceImpl implements SBGTCDService {
 	
 	private final SBGTCDMapper mapper;
@@ -31,9 +33,7 @@ public class SBGTCDServiceImpl implements SBGTCDService {
 
 	@Override
 	public void deleteRow(String bgtCd) {
-		System.out.println("������������");
 		mapper.deleteRow(bgtCd);
-		
 	}
 
 }
