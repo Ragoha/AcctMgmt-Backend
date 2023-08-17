a98b6a98bbb62e571c824efbc6e8b013340e6cf9
ragoha92 <ragoha92@naver.com>
2023-08-17 14:45:27 +0900

    fix. 예산과목 수정

--- src/main/java/kr/co/acctmgmt/mapper/BgtGrMapper.java
+++ src/main/java/kr/co/acctmgmt/mapper/BgtGrMapper.java
@@ -7,5 +7,11 @@ import kr.co.acctmgmt.domain.BgtGr;
 public interface BgtGrMapper {
 	
 	public List<BgtGr> findBgtGrByCoCdAndKeyword(BgtGr bgtGr);
+
+	public void deleteBgtGr(BgtGr bgtgr);
+
+	public void updateBgtGr(BgtGr bgtgr);
+
+	public void insertBgtGr(BgtGr bgtgr);
 	
 }
