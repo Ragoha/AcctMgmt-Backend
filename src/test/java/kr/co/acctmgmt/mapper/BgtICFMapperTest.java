234eaeac0ba2f7683734638466c0daa081c50fac
ragoha92 <ragoha92@naver.com>
2023-08-18 17:13:36 +0900

    fix. mapperTest 주석

--- src/test/java/kr/co/acctmgmt/mapper/BgtICFMapperTest.java
+++ src/test/java/kr/co/acctmgmt/mapper/BgtICFMapperTest.java
@@ -39,79 +39,79 @@ public class BgtICFMapperTest {
 	
 //	@Test
 	public void insertBgtICFTest() {
-		BgtICF bgticf = BgtICF.builder()
-	            .coCd(1)
-	            .gisu(456)
-	            .sq(5)
-	            .divCd(1000)
-	            .deptCd(1000)
-	            .mgtCd(1000)
-	            .bgtCd("BGT005")
-	            .bgtFg(1)
-	            .bottomNm("BT005")
-	            .carrAm((long) 1900.65)
-	            .carrAm1((long) 1600.65)
-	            .carrAm2((long) 2400.55)
-	            .carrAm3((long) 2100.75)
-	            .empCd(1000)
-	            .remDc("Dummy description 5")
-	            .bgtTy("A")
-	            .insertId("ID005")
-	            .insertDt(new Date())
-	            .insertIp("127.0.0.1")
-	            .modifyId("ID005")
-	            .modifyDt(new Date())
-	            .modifyIp("127.0.0.1")
-	            .build();
-
-	    bgticfMapper.insertBgtICF(bgticf);
-	    
-	    getBgtICFListTest();
+//		BgtICF bgticf = BgtICF.builder()
+//	            .coCd("1")
+//	            .gisu(456)
+//	            .sq(5)
+//	            .divCd("1000")
+//	            .deptCd(1000)
+//	            .mgtCd(1000)
+//	            .bgtCd("BGT005")
+//	            .bgtFg(1)
+//	            .bottomNm("BT005")
+//	            .carrAm((long) 1900.65)
+//	            .carrAm1((long) 1600.65)
+//	            .carrAm2((long) 2400.55)
+//	            .carrAm3((long) 2100.75)
+//	            .empCd(1000)
+//	            .remDc("Dummy description 5")
+//	            .bgtTy("A")
+//	            .insertId("ID005")
+//	            .insertDt(new Date())
+//	            .insertIp("127.0.0.1")
+//	            .modifyId("ID005")
+//	            .modifyDt(new Date())
+//	            .modifyIp("127.0.0.1")
+//	            .build();
+//
+//	    bgticfMapper.insertBgtICF(bgticf);
+//	    
+//	    getBgtICFListTest();
 	}
 
     
 //	@Test
 	public void updateBgtICFTest() {
-		BgtICF bgticf = BgtICF.builder()
-	            .coCd(1)
-	            .gisu(456)
-	            .sq(5)
-	            .divCd(1000)
-	            .deptCd(1000)
-	            .mgtCd(1000)
-	            .bgtCd("BGT005")
-	            .bgtFg(1)
-	            .bottomNm("BT005")
-	            .carrAm((long) 1900.65)
-	            .carrAm1((long) 1600.65)
-	            .carrAm2((long) 2400.55)
-	            .carrAm3((long) 2100.75)
-	            .empCd(1000)
-	            .remDc("Dummy description 5")
-	            .bgtTy("A")
-	            .insertId("ID005")
-	            .insertDt(new Date())
-	            .insertIp("127.0.0.1")
-	            .modifyId("ID005")
-	            .modifyDt(new Date())
-	            .modifyIp("127.0.0.1")
-	            .build();
-
-	    bgticfMapper.updateBgtICF(bgticf);
-	    
-	    // 업데이트 후 데이터 조회
-	    getBgtICFListTest();
+//		BgtICF bgticf = BgtICF.builder()
+//	            .coCd(1)
+//	            .gisu(456)
+//	            .sq(5)
+//	            .divCd(1000)
+//	            .deptCd(1000)
+//	            .mgtCd(1000)
+//	            .bgtCd("BGT005")
+//	            .bgtFg(1)
+//	            .bottomNm("BT005")
+//	            .carrAm((long) 1900.65)
+//	            .carrAm1((long) 1600.65)
+//	            .carrAm2((long) 2400.55)
+//	            .carrAm3((long) 2100.75)
+//	            .empCd(1000)
+//	            .remDc("Dummy description 5")
+//	            .bgtTy("A")
+//	            .insertId("ID005")
+//	            .insertDt(new Date())
+//	            .insertIp("127.0.0.1")
+//	            .modifyId("ID005")
+//	            .modifyDt(new Date())
+//	            .modifyIp("127.0.0.1")
+//	            .build();
+//
+//	    bgticfMapper.updateBgtICF(bgticf);
+//	    
+//	    // 업데이트 후 데이터 조회
+//	    getBgtICFListTest();
 	}
     
 //    @Test
     public void deleteBgticfTest() {
-    	BgtICF bgticf = BgtICF.builder()
-        		.coCd(1)
-        		.gisu(456)
-        		.build();
-        
-        bgticfMapper.deleteBgtICF(bgticf);
-        
-        getBgtICFListTest();
+//    	BgtICF bgticf = BgtICF.builder()
+//        		.coCd(1)
+//        		.gisu(456)
+//        		.build();
+//        
+//        bgticfMapper.deleteBgtICF(bgticf);
+//        
+//        getBgtICFListTest();
     }
 }
