fd2060a294730f3edf0651d0e6273285df1c526d
ragoha92 <ragoha92@naver.com>
2023-08-29 14:11:32 +0900

    FIX

--- src/test/java/kr/co/acctmgmt/mapper/CoMapperTest.java
+++ src/test/java/kr/co/acctmgmt/mapper/CoMapperTest.java
@@ -1,47 +1,42 @@
-import java.util.List;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import kr.co.acctmgmt.domain.Co;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:applicationContext.xml" })
-public class CoMapperTest {
-	
-	@Autowired
-	private CoMapper coMapper;
-	
-//	@Test
-	public void getCo(){
-		int coCd = 1;
-		
-//		List<Co> sco= coMapper.getCo(coCd);
-//		System.out.println(sco.toString());
-	}
-	
-//	@Test
-	public void getNum() {
-		int num = coMapper.getNum();
-		System.out.println(num);
-	}
-	
-
-//	@Test
-//	public void deleteCo() {
-//		int coCd = 2;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = { "classpath:applicationContext.xml" })
+//public class CoMapperTest {
+//	
+//	@Autowired
+//	private CoMapper coMapper;
+//	
+////	@Test
+//	public void getCo(){
+//		int coCd = 1;
 //		
-//		coMapper.deleteCo("coCd");
+////		List<Co> sco= coMapper.getCo(coCd);
+////		System.out.println(sco.toString());
 //	}
-//	@Test
-//	public void updateCo() {
-//		int coCd = 1007;
-
-//		String coAddr="¼­ÃÊ±¸";
-//		coMapper.updateCo(coCd);
+//	
+////	@Test
+//	public void getNum() {
+//		int num = coMapper.getNum();
+//		System.out.println(num);
 //	}
-
-}
+//	
+//
+////	@Test
+////	public void deleteCo() {
+////		int coCd = 2;
+////		
+////		coMapper.deleteCo("coCd");
+////	}
+////	@Test
+////	public void updateCo() {
+////		int coCd = 1007;
+//
+////		String coAddr="¼­ÃÊ±¸";
+////		coMapper.updateCo(coCd);
+////	}
+//
+//}
